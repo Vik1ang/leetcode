21e9b0f2480aa9e2832ebc5326466d56264f025d
Vik1ang <vik1ang@outlook.com>
2022-07-30 22:12:52 +0800

    leetcode for cpp

---
+++ Cpp/src/leetcode.h
@@ -0,0 +1,14 @@
+#ifndef LEETCODE_H
+#define LEETCODE_H
+
+#pragma region hashtable
+
+#include "hashtable/q1.cpp"
+
+#pragma endregion
+
+#pragma region tree
+#include "tree/q94.cpp"
+#pragma endregion
+
+#endif // LEETCODE_H
---
+++ Cpp/src/tree/q94.cpp
@@ -0,0 +1,11 @@
+#include "data_structure.h"
+
+namespace q94 {
+    class Solution1 {
+        // 递归
+    public:
+        std::vector<int> inorderTraversal(TreeNode *root) {
+            return {};
+        }
+    };
+}
