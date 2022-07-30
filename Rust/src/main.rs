5753d2fb5c56e756e7755cd51ff2f1765d8f2773
Vik1ang <vik1ang@outlook.com>
2022-07-31 01:49:52 +0800

    q1

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -4,6 +4,6 @@ mod tree;
 
 fn main() {
     println!("Hello, world!");
-    let a = hashtable::q1::Solution1::two_sum(vec![1, 2, 3], 59);
+    let a = hashtable::q1::Solution1::two_sum(vec![2, 7, 11, 15], 9);
     println!("{:?}", a);
 }
