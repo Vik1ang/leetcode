dbe968fb71342b43233f2ec6a499e4927eb25c87
Vik1ang <vik1ang@outlook.com>
2022-08-03 23:27:54 +0800

    q3

--- Cpp/src/slidewindow/q3.cpp
+++ Cpp/src/slidewindow/q3.cpp
@@ -1,5 +1,6 @@
 ﻿#include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 namespace q3 {
     class Solution1 {
@@ -10,15 +11,13 @@ namespace q3 {
             int left = 0, right = 0;
             int res = 0;
 
-            while (right < s.size())
-            {
+            while (right < s.size()) {
                 char c = s[right];
                 right++;
                 // 更新窗口内数据
                 window[c]++;
                 // 判断左侧窗口是否要收缩
-                while (window[c] > 1)
-                {
+                while (window[c] > 1) {
                     char d = s[left];
                     left++;
                     // 进行窗口内数据的更新
@@ -31,4 +30,42 @@ namespace q3 {
             return res;
         }
     };
+
+    class Solution2 {
+    public:
+        int lengthOfLongestSubstring(std::string s) {
+            int left = 0, right = 0, res = 0;
+            std::unordered_set<char> window;
+            for (int i = 0; i < s.length(); ++i) {
+                char ch = s[i];
+                while (window.count(ch) == 1) {
+                    window.erase(s[left++]);
+                }
+                window.insert(ch);
+                res = std::max(res, right - left + 1);
+                right++;
+            }
+
+            return res;
+        }
+    };
+
+    class Solution3 {
+    public:
+        int lengthOfLongestSubstring(std::string s) {
+            int left = 0, res = 0;
+            int window[128] = {0};
+
+
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s[i];
+                left = std::max(left, window[ch]);
+                res = std::max(res, i - left + 1);
+                window[ch] = i + 1;
+            }
+
+            return res;
+        }
+    };
+
 }
