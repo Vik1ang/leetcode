7c2fab50355a51e06ce35748e67efbcf64a19acb
Vik1ang <vik1ang@outlook.com>
2022-11-11 09:59:39 +0800

    restart

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,11 +1,1 @@
-mod data_structure;
-mod hashtable;
-mod linkedlist;
-mod slidewindow;
-mod tree;
 
-fn main() {
-    println!("Hello, world!");
-    let a = hashtable::q1::Solution1::two_sum(vec![2, 7, 11, 15], 9);
-    println!("{:?}", a);
-}
